57b80ccef9a4de0a58e0e0ad36eacf5833532f66
Serga05 <88805832+Serga05@users.noreply.github.com>
2021-12-11 01:21:09 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,3 @@
+       function SIL(){
+		   document.write('<a href="https://serga05.github.io/SMM-YSLYGI/"><button class="button-BACK"><-- Назад</button></a><p><link href="index.css" rel="stylesheet"></link>Так же имеется сотрудничество с блогерами и настройка таргетированной рекламы');
+	   }
